bf8e1d37e3412c03510b65a3b49a146caa07f2a5
Alejandro <aleagf@hotmail.com>
2022-07-05 15:56:34 -0400

    commit

--- src/routes/links.js
+++ src/routes/links.js
@@ -2,5 +2,14 @@ const express = require('express');
 const router = express.Router();
 const pool = require ('../database');
 
-//fneriugbeivberv
+router.get('/RegistroVacuna', async(req, res, next) => {
+    res.render('links/ProductoModificar', { Query: Query[0] })
+});
+
+/*
+router.post('/RegistroVacuna', async(req, res, next) => {
+    res.render('links/ProductoModificar', { Query: Query[0] })
+});
+*/
+
 module.exports=router;
