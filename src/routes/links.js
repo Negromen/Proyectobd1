3dd6be3fb2d88f8c3ee4b376392f0289ba5e9702
Alejandro <aleagf@hotmail.com>
2022-07-06 17:26:31 -0400

    Sirve para pegar en select vacuna

--- src/routes/links.js
+++ src/routes/links.js
@@ -4,14 +4,23 @@ const pool = require('../database');
 
 ////--------------------------------------------------------REDIRECCCIONAMIENTO--------------------------------------------------
 
-router.get('/registrarVacuna', (req , res) => {
+router.get('/registrarVacuna', async(req , res, next) => {
+    const Query = await pool.query("select nombrevacuna from vacuna");
+    //const Query2 = await pool.query("select cs.nombrecentro from centro_salud as cs, centro_vacunacion as cv where cs.codcentro = cv.codcentro and cs.codestado =cv.codestado and cs.codpais = cv.codpais");
+    console.log(Query);
+    if((Query))
+    res.render("links/registrarVacuna", {Query});
+    else
     res.render("links/registrarVacuna");
 });
 
 router.post('/registrarVacuna', async (req , res, next) => {
-    
+
     res.render("links/registrarVacuna");
 });
 
+router.get('/verificarRegistro', (req , res) => {
+    res.render("links/verificarRegistro");
+});
 
 module.exports = router;
