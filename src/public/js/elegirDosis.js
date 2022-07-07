52cc2a1bb89b32129caad7cd36e2cb7ed4d256a6
Alejandro <aleagf@hotmail.com>
2022-07-06 21:06:43 -0400

    ajaaa

--- src/public/js/elegirDosis.js
+++ src/public/js/elegirDosis.js
@@ -1,12 +1,15 @@
-const pool = require('/src/database');
+const express = require('express');
+const router = express.Router();
+const pool = require('../database');
 
-function elegirDosis() {
+
+async function elegirDosis() {
     let vacuna = document.getElementById('inputVacuna');
     let idvacuna = vacuna.value;
-    const Query = await pool.query("select cantdosis from vacuna where idvacuna = ? ", [idvacuna]);
-    console.log("este es el query", Query);
-    document.getElementById('inputDosis').value = Query[0];
-    const valor = Query[0];
+    //const Query = await pool.query("select cantdosis from vacuna where idvacuna = ? ", [idvacuna]);
+    //console.log(Query);
+    document.getElementById('inputDosis').value = 2;
+    const valor = 2;
     const select = document.getElementById('inputDosis');
 
     for (var i = 1; i <= valor; i++) {
--- src/routes/links.js
+++ src/routes/links.js
@@ -2,12 +2,31 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database');
 
+////--------------------------------------------------------FUNCIONES------------------------------------------------------------
+
+
+function elegirDosis() {
+    let vacuna = document.getElementById('inputVacuna');
+    let idvacuna = vacuna.value;
+    //const Query = await pool.query("select cantdosis from vacuna where idvacuna = ? ", [idvacuna]);
+    //console.log("este es el query", Query);
+    document.getElementById('inputDosis').value = 4;
+    const valor = 4;
+    const select = document.getElementById('inputDosis');
+    for (var i = 1; i <= valor; i++) {
+        const option = document.createElement('option');
+        option.text = i;
+        select.appendChild(option);
+    }
+    document.getElementById('divDosis').style.display = 'block';
+};
+
+
 ////--------------------------------------------------------REDIRECCCIONAMIENTO--------------------------------------------------
 
 router.get('/registrarVacuna', async(req , res, next) => {
     const Query = await pool.query("select idvacuna,nombrevacuna from vacuna");
     //const Query2 = await pool.query("select cs.nombrecentro from centro_salud as cs, centro_vacunacion as cv where cs.codcentro = cv.codcentro and cs.codestado =cv.codestado and cs.codpais = cv.codpais");
-    console.log(Query);
     if((Query))
     res.render("links/registrarVacuna", {Query});
     else
@@ -15,7 +34,6 @@ router.get('/registrarVacuna', async(req , res, next) => {
 });
 
 router.post('/registrarVacuna', async (req , res, next) => {
-
     res.render("links/registrarVacuna");
 });
 
