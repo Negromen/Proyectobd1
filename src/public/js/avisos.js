e23f8a5439240507c4d3bc3e03e081c510ef4838
Iagovisk <66756953+Iagovisk@users.noreply.github.com>
2022-07-20 00:57:22 -0400

    Agregado botones en navBar

--- src/public/js/avisos.js
+++ src/public/js/avisos.js
@@ -7,7 +7,8 @@ function alertaRegistroVacuna() {
     let estado = document.getElementById('inputEstado').value;
     let municipio = document.getElementById('inputMunicipio').value;
     let cedula = document.getElementById('inputCedula').value;
-    let tipocedula = document.getElementById('tipoCedula').value
+    let tipocedula = document.getElementById('tipoCedula');
+    tipocedula.disabled = false;
     let fechanac = document.getElementById('inputDate').value;
     let genero = document.getElementById('inputGenero').value;
     let vacuna = document.getElementById('inputVacuna').value;
@@ -15,7 +16,7 @@ function alertaRegistroVacuna() {
     let fechaVac = document.getElementById('inputDateV').value;
     let centroSalud = document.getElementById('inputCS').value;
     let personalSalud = document.getElementById('inputPS').value;
-    if ((nombre !== '') && (apellido !== '') && (pais !== '') && (estado !== '') && (municipio !== '') && (tipocedula !== '') && (cedula !== '') && (cedula > 0) && (fechanac !== '') && (genero !== '') && (vacuna !== '') && (numDosis !== '') && (fechaVac !== '') && (centroSalud !== '') && (personalSalud !== '')) {
+    if ((nombre !== '') && (apellido !== '') && (pais !== '') && (estado !== '') && (municipio !== '') && (tipocedula.value !== '') && (cedula !== '') && (cedula > 0) && (fechanac !== '') && (genero !== '') && (vacuna !== '') && (numDosis !== '') && (fechaVac !== '') && (centroSalud !== '') && (personalSalud !== '')) {
         Swal.fire(
             'Felicidades!',
             'Se ha registrado correctamente!',
