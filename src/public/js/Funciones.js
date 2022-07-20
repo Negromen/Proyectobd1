4cc87b296801bf98ed43c9434adf10fd84f8d8e7
Alejandro <aleagf@hotmail.com>
2022-07-20 15:54:17 -0400

    FALTA ANADIR CENTRO

--- src/public/js/Funciones.js
+++ src/public/js/Funciones.js
@@ -20,7 +20,7 @@ async function elegirDosis() {
     document.getElementById('divDosis').style.display = 'block';
 };
 
-//PARA ELEGIR EL PEROSNAL DE SALUD DE UN CENTRO 
+//PARA ELEGIR EL PERSONAL DE SALUD DE UN CENTRO 
 async function elegirPS() {
     let cs = document.getElementById('inputCS');
     let valueCS = cs.value;
@@ -85,38 +85,38 @@ async function elegirMunicipio() {
 async function buscarCentro() {
     let centro = document.getElementById('buscarCodigo');
     let codcentro = centro.value;
-    console.log("codcentro", codcentro);
     let response = await fetch(`http://localhost:4000/links/buscamecentro/${codcentro}`);
     let response2 = await response.json();
     document.getElementById('inputNombreC').value = response2[0].nombrecentro;
     document.getElementById('inputDireccionC').value = response2[0].direccion;
     document.getElementById('fechaEncargado').value = response2[0].fechaEncargado;
     document.getElementById('inputPais').options[0].text = response2[0].nombrepais;
+    document.getElementById('inputPais').options[0].value = response2[0].codpais;
     document.getElementById('inputEstado').options[0].text = response2[0].nombreestado;
+    document.getElementById('inputEstado').options[0].value = response2[0].codestado;
     document.getElementById('medico').options[0].text = response2[0].nombreper + ' ' + response2[0].apellidoper;
     document.getElementById('medico').options[0].value = response2[0].docidentidad;
     document.getElementById('tipoCentro').options[0].text = response2[0].tipo;
+    document.getElementById('tipoCentro').options[0].value= response2[0].tipo;
     document.getElementById('botonborrar').disabled = false;
     document.getElementById('botoneditar').disabled = false;
 };
 
-
 async function editarCentro() {
+    document.getElementById('botonBuscar').disabled = true;
     document.getElementById('inputNombreC').disabled = false;
     document.getElementById('inputDireccionC').disabled = false;
-    document.getElementById('tipoCentro').disabled = false;
     document.getElementById('tipoCentro').disabled = false;
     document.getElementById('medico').disabled = false;
     document.getElementById('botonguardar').disabled = false;
     codcentro = document.getElementById('buscarCodigo').value;
-    console.log(codcentro);
     var cedula = document.getElementById('medico').value;
     let response = await fetch(`http://localhost:4000/links/buscadoctores/${codcentro}`);
     let response2 = await response.json();
     const valor = Object.keys(response2).length;
     const select = document.getElementById('medico');
     for (i = 1; i <= valor; i++) {
-        console.log(response2[i - 1].nombreper + ' ' + response2[i - 1].apellidoper)
+        //console.log(response2[i - 1].nombreper + ' ' + response2[i - 1].apellidoper)
         if ((response2[i - 1].docidentidad) !== cedula) {
             const option = document.createElement('option');
             option.value = response2[i - 1].docidentidad;
@@ -126,20 +126,56 @@ async function editarCentro() {
     }
 };
 
-/*
+
 async function GuardarEditarCentro(){
     var centro={
-        codcentro:,
-        nombrecentro:,
-        direccion:,
-        codestado:,
-        codpais:,
-        docidentidad:,
-        fechaEncargado
-    }
-        document.getElementById('botonguardar').disabled = true;
+        codcentro:document.getElementById('buscarCodigo').value,
+        nombrecentro:document.getElementById('inputNombreC').value,
+        direccion:document.getElementById('inputDireccionC').value,
+        codestado:document.getElementById('inputEstado').options[0].value,
+        codpais:document.getElementById('inputPais').options[0].value,
+        docidentidad:document.getElementById('medico').options[0].value,
+        fechaEncargado:document.getElementById('fechaEncargado').value,
+        tipo:document.getElementById('tipoCentro').value
+    };
+    await fetch(`http://localhost:4000/links/GuardarEditarCentro/`,{
+        method:'POST',
+        body:JSON.stringify(centro),
+        headers:{
+            "Content-type":"application/json"
+        }
+    });
+    console.log("SALIO DEL FETCH");
+    document.getElementById('inputNombreC').disabled = true;
+    document.getElementById('inputDireccionC').disabled = true;
+    document.getElementById('tipoCentro').disabled = true;
+    document.getElementById('medico').disabled = true;
+    document.getElementById('botonguardar').disabled = true;
+    document.getElementById('botonBuscar').disabled = false;
 };
-*/
+
+async function borraCentro(){
+    var centro={
+        codcentro:document.getElementById('buscarCodigo').value,
+        nombrecentro:document.getElementById('inputNombreC').value,
+        direccion:document.getElementById('inputDireccionC').value,
+        codestado:document.getElementById('inputEstado').options[0].value,
+        codpais:document.getElementById('inputPais').options[0].value,
+        docidentidad:document.getElementById('medico').options[0].value,
+        fechaEncargado:document.getElementById('fechaEncargado').value,
+        borrado:true
+    };
+    await fetch(`http://localhost:4000/links/borrarCentro/`,{
+        method:'POST',
+        body:JSON.stringify(centro),
+        headers:{
+            "Content-type":"application/json"
+        }
+    });
+    console.log("salio del fetch que borra");
+    window.location = "http://localhost:4000/links/controlCentroSalud"
+}
+
 
 function editarfechaEncargado() {
     let date = new Date();
@@ -167,17 +203,3 @@ function aparecer() {
         boton.style.display = 'none';
     }, false);
 };
-
-/*  FUNCION SIN USO AUN
-function habilitarCampos() {
-    document.getElementById('inputNombre').disabled = false;
-    document.getElementById('inputApellido').disabled = false;
-    document.getElementById('inputPais').disabled = false;
-    document.getElementById('inputEstado').disabled = false;
-    document.getElementById('inputMunicipio').disabled = false;
-    document.getElementById('inputCedula').disabled = false;
-    document.getElementById('inputDate').disabled = false;
-    document.getElementById('inputGenero').disabled = false;
-    document.getElementById('divBtnRegistro').style.display = 'block';
-};
-*/
